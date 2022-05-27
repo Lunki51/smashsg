cb43f4e263d15e96ff3898fede6b41ac13efd765
Lunki <quentincosnier@free.fr>
2022-05-28 00:04:19 +0200

    images text download poc'han css

--- main.js
+++ main.js
@@ -12,6 +12,10 @@ app.get("/",async(req,reply)=>{
     reply.sendFile('./index.html');
 })
 
+app.get("/poc'han",async(req,reply)=>{
+    reply.send("Donnez de l'argent a poc'han svp")
+})
 
 
-app.listen(3000)
+
+app.listen(4015,"0.0.0.0")
